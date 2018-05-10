a29e2e69cacfc8fcdb5b33f2c63eab6076fcd56c
blueilla <jasenszky.ilona@gmail.com>
2018-05-10 14:00:43 +0200

    first few exercises

---
+++ week-01/day-04/src/Integers.java
@@ -0,0 +1,8 @@
+public class Integers {
+    public static void main(String[] args) {
+        System.out.println(-2147483647);
+        System.out.println(-1.5);
+        System.out.println(.5);
+
+    }
+}
---
+++ week-01/day-04/src/IntroduceYourself.java
@@ -0,0 +1,8 @@
+public class IntroduceYourself {
+
+    public static void main(String[] args) {
+        System.out.println("Jasenszky Ilona");
+        System.out.println(33);
+        System.out.println(1.67);
+    }
+}
