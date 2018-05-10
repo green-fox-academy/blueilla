c920d1a0a53e812b10b2e8ce2e89381a97ee8fea
blueilla <jasenszky.ilona@gmail.com>
2018-05-10 16:27:56 +0200

    coding ours and favorite numbers exersises

--- week-01/day-04/src/CodingHours.java
+++ week-01/day-04/src/CodingHours.java
@@ -1,13 +1,13 @@
 public class CodingHours {
     public static void main(String[] args) {
         int CodePerDay = 6;
-        int WeekCoding = 5 * 6;
+        int WeekCoding = 5 * CodePerDay;
         int SemesterWeeks = 17;
-        int AverageHours = 52;
+        int FullTime = SemesterWeeks * 52;
 
-        System.out.println((CodePerDay + WeekCoding) * SemesterWeeks);
-        System.out.println()
-
+        System.out.println(WeekCoding * SemesterWeeks);
+        System.out.println(WeekCoding / 52. * 100);
+        System.out.println(FullTime);
 
     }
 
