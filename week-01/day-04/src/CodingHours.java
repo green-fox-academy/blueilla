84dbd233cf8b69a9c9cae3008d539def793ddaf3
blueilla <jasenszky.ilona@gmail.com>
2018-05-10 14:33:34 +0200

    CodingHours

---
+++ week-01/day-04/src/CodingHours.java
@@ -0,0 +1,14 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        int CodePerDay = 6;
+        int WeekCoding = 5 * 6;
+        int SemesterWeeks = 17;
+        int AverageHours = 52;
+
+        System.out.println((CodePerDay + WeekCoding) * SemesterWeeks);
+        System.out.println()
+
+
+    }
+
+}
