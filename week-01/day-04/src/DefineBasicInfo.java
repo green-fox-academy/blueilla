ba259d35d0ef277639e55e4fdc5afd4f53625654
blueilla <jasenszky.ilona@gmail.com>
2018-05-10 17:31:23 +0200

    further exercises

---
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -0,0 +1,10 @@
+public class DefineBasicInfo{
+
+    public static void main(String[] args) {
+        String name = "Ilona";
+        int age = 33;
+        double height = 1.68;
+        boolean married = false;
+
+    }
+}
