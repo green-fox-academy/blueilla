5a530c5bf63443b7db75b198c63ef845911dec56
blueilla <jasenszky.ilona@gmail.com>
2018-05-10 13:34:32 +0200

    HelloOthers an HumptyDumpty

---
+++ week-01/day-04/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Dome!");
+        System.out.println("Hello Boldi!");
+        System.out.println("Hello Isti!");
+    }
+}
