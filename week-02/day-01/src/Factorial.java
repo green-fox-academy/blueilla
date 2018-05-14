3a54333f035e06d28bf6ae71ca9044432db23afb
blueilla <jasenszky.ilona@gmail.com>
2018-05-14 14:42:28 +0200

    factorial exercise

---
+++ week-02/day-01/src/Factorial.java
@@ -0,0 +1,20 @@
+public class Factorial {
+
+    public static void main(String[] args) {
+        int mynumber = 3;
+        System.out.println(factorio(mynumber));
+
+
+
+    }
+    public static int factorio(int number) {
+        int result = 1;
+        for (int i = 1; i <= number ; i++) {
+            result *= i;
+
+        }
+        return result;
+    }
+
+
+}
