4a15d18ba87a767171947976714a613a98952f30
blueilla <jasenszky.ilona@gmail.com>
2018-05-14 17:32:09 +0200

    AppendAa exercise

---
+++ week-02/day-01/src/AppendAa.java
@@ -0,0 +1,11 @@
+public class AppendAa {
+
+    public static void main(String[] args) {
+
+     String[] animals = {"kuty", "macsk", "cic"};
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[i] + "a");
+
+        }
+    }
+}
