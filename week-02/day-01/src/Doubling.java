10aa68c6c031f8ecd282c4001b5eb05b06adb493
blueilla <jasenszky.ilona@gmail.com>
2018-05-14 13:42:46 +0200

    doubling exercise

---
+++ week-02/day-01/src/Doubling.java
@@ -0,0 +1,15 @@
+public class Doubling {
+
+    public static void main(String[] args) {
+
+        int baseNum = 543;
+        doubling(baseNum);
+        doubling(321);
+        System.out.println(doubling(baseNum));
+    }
+
+        public static int doubling(int number) {
+        return(number * 2);
+    }
+
+}
