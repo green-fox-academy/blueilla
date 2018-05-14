e78fa81a4216a767750329a79ffe14603fe60d81
blueilla <jasenszky.ilona@gmail.com>
2018-05-14 17:15:59 +0200

    Double Item ex.

---
+++ week-02/day-01/src/DoubleItem.java
@@ -0,0 +1,11 @@
+public class DoubleItem {
+
+    public static void main(String[] args) {
+
+        int[] numList = {3, 4, 5, 6, 7};
+        for (int i = 0; i < numList.length; i++) {
+            System.out.println(numList[i] * 2);
+
+        }
+    }
+}
