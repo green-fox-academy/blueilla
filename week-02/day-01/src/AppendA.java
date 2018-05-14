fbb0810808358c0a84b636b58b3d0c3d1395a128
blueilla <jasenszky.ilona@gmail.com>
2018-05-14 14:00:10 +0200

    AppendA exercise

---
+++ week-02/day-01/src/AppendA.java
@@ -0,0 +1,13 @@
+public class AppendA {
+
+    public static void main(String[] args) {
+
+        String am = "kuty";
+        appendA(am);
+
+    }
+
+    public static void appendA(String text) {
+        System.out.println(text + "a");
+    }
+}
