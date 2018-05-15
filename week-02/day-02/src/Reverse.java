2a4218c8c522ddf797f08e17747c5f8822a0bd99
blueilla <jasenszky.ilona@gmail.com>
2018-05-15 14:38:24 +0200

    Reverse without function

--- week-02/day-02/src/Reverse.java
+++ week-02/day-02/src/Reverse.java
@@ -1,6 +1,18 @@
 public class Reverse {
 
-    public static void main(String[] args) {
-        
+    public static void main(String... args) {
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+
+        String normal = "";
+        for (int i = 0; i < reversed.length(); i++) {
+            normal += reversed.charAt(reversed.length() -1 - i);
+
+        }
+        System.out.println(normal);
+
+       // System.out.println(reverse(reversed));
     }
+
+
 }
