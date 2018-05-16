7337ee42f127c9a129eee1f8bdc3b22aa4c78b21
blueilla <jasenszky.ilona@gmail.com>
2018-05-16 15:58:42 +0200

    WriteSingleLine ex.

---
+++ week-02/day-03/src/WriteSingleLine.java
@@ -0,0 +1,20 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteSingleLine {
+
+    public static void main(String[] args) {
+        List<String> content = new ArrayList<>();
+        content.add("Jasenszky Ilona");
+        try {
+            Path filePath = Paths.get("my-file.txt");
+            Files.write(filePath, content);
+
+        } catch (Exception e){
+            System.out.println("Unable to write file: my-file.txt");
+        }
+    }
+}
