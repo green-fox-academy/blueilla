2682dcbdf0f4911aaff7b1efd485461d4079bc16
blueilla <jasenszky.ilona@gmail.com>
2018-05-16 13:47:31 +0200

    PrintEachLine ex.

---
+++ week-02/day-03/src/PrintEachLine.java
@@ -0,0 +1,21 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
+
+public class PrintEachLine {
+
+    public static void main(String[] args) {
+
+        try {
+            Path filePath = Paths.get("my-file.txt");
+            List<String> lines = Files.readAllLines(filePath);
+            for (int i = 0; i < lines.size() ; i++) {
+                System.out.println(lines.get(i));
+                }
+            } catch (Exception e) {
+            System.out.println("Uh-oh, could not read the file!");
+        }
+
+    }
+}
