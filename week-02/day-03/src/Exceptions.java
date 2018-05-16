f82f8c2ba2c6ea43b5a85d2fb97e462db5d303be
blueilla <jasenszky.ilona@gmail.com>
2018-05-16 13:11:58 +0200

    divide by zero exception

---
+++ week-02/day-03/src/Exceptions.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exceptions {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int divisor = scanner.nextInt();
+
+        try {
+            int result = 10 / divisor;
+            System.out.println(result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Can't divide by zero!");
+        }
+    }
+}
