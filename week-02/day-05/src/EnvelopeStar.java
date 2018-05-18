012116d08fae4b31f78353c80f284a28961dc85c
blueilla <jasenszky.ilona@gmail.com>
2018-05-18 17:47:26 +0200

    start new exercises(Envelope Stars)

--- week-02/day-05/src/EnvelopeStar.java
+++ week-02/day-05/src/EnvelopeStar.java
@@ -4,33 +4,35 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class LinePlay {
-    public static void mainDraw(Graphics graphics) {
 
-        int xStart = 10;
-        int yStart = 5;
-        int xEnd = WIDTH - 5;
-        int yEnd = 10;
+public class EnvelopeStar {
+    public static void mainDraw(Graphics graphics) {
+        int startingPoint1 = 0;
+        int startingPoint2 = 0;
+
+        int endPoint1 = WIDTH;
+        int endPoint2 = 10;
         int lineCount = 15;
 
+        for (int i = 0; i < lineCount ; i++) {
+            drawLine(startingPoint1, startingPoint2, endPoint1, endPoint2, Color.magenta, graphics);
+            startingPoint1 += startingPoint1;
+            endPoint2 += endPoint2;
 
-        for (int i = 0; i < lineCount; i++) {
-            drawLine(xStart, yStart, xEnd, yEnd, Color.pink, graphics);
-            drawLine(yStart, xStart, yEnd, xEnd, Color.blue, graphics);
-            xStart += WIDTH / lineCount;
-            yEnd += HEIGHT / lineCount;
         }
-
     }
 
+
+
+
     public static void drawLine(int x1, int y1, int x2, int y2, Color color, Graphics graphics) {
-    graphics.setColor(color);
-    graphics.drawLine(x1, y1, x2, y2);
+        graphics.setColor(color);
+        graphics.drawLine(x1, y1, x2, y2);
     }
 
     // Don't touch the code below
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
+    static int WIDTH = 400;
+    static int HEIGHT = 400;
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
