15333a6dcb1f2ce6404875d061ffaa93eb2ec6a0
blueilla <jasenszky.ilona@gmail.com>
2018-05-23 15:57:17 +0200

    DiceSet exercise

--- week-03/day-03/Dominoes/src/Dominoes.java
+++ week-03/day-03/Dominoes/src/Dominoes.java
@@ -7,7 +7,6 @@ public class Dominoes {
         // You have the list of Dominoes
         // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
         // eg: [2, 4], [4, 3], [3, 5] ...
-
         List<Domino> sortedDominoes = new ArrayList<>();
         sortedDominoes.add(dominoes.get(0));
 
@@ -16,10 +15,9 @@ public class Dominoes {
                 if (sortedDominoes.get(i).getValues()[1] == dominoes.get(j).getValues()[0]){
                     sortedDominoes.add(dominoes.get(j));
                 }
-
             }
-
         }
+
         System.out.println(sortedDominoes);
     }
 
