2f3a6b7c5ef557c20ca5b77b16362a12b3aee906
blueilla <jasenszky.ilona@gmail.com>
2018-05-25 09:42:50 +0200

    feat(Squares) biggest square and left middle square

--- week-03/day-04/Recursion/src/StringXtoY.java
+++ week-03/day-04/Recursion/src/StringXtoY.java
@@ -1,10 +1,3 @@
 public class StringXtoY {
-    public static void main(String[] args) {
-        // Given a string, compute recursively (no loops) a new string where all the
-// lowercase 'x' chars have been changed to 'y' chars.
-        String newString = newStringFunction("exex");
-    }
-    public static String newStringFunction(String string) {
-        if 
-    }
+
 }
---
+++ week-03/day-05/fractals/src/Squares.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Squares {
+    public static void mainDraw(Graphics graphics){
+      int size = WIDTH;
+      int basicX = 0;
+      int basicY = 0;
+      drawSquare(basicX, basicY, size, graphics);
+      drawFourSquares(basicX, WIDTH / 3, HEIGHT / 3, graphics);
+
+    }
+    public static void drawSquare(int x, int y, int size, Graphics graphics) {
+        graphics.setColor(Color.black);
+        graphics.drawRect(x, y, size, size);
+    }
+
+    public static void drawFourSquares(int x, int y, int size, Graphics graphics) {
+        for (int i = 0; i < 1 ; i++) {
+            graphics.setColor(Color.black);
+            graphics.drawRect(x, WIDTH / 3, WIDTH / 3, HEIGHT / 3);
+
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 400;
+    static int HEIGHT = 400;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT + 23));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
