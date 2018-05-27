1a903cf2a60e8daa95fe94a7e5f4d1c5a75a0cc4
blueilla <jasenszky.ilona@gmail.com>
2018-05-27 14:46:08 +0200

    feat(Logs)try some function

--- week-02/day-03/src/Logs.java
+++ week-02/day-03/src/Logs.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Logs {
@@ -12,13 +13,22 @@ public class Logs {
 // Each line represents a log message from a web server
 // Write a function that returns an array with the unique IP adresses.
 // Write a function that returns the GET / POST request ratio.
-        Path filePath = Paths.get("./log.txt");
-        List<String> lines = null;
         try {
-            lines = Files.readAllLines(filePath);
-            System.out.println(lines.get(0));
+            Path filePath = Paths.get("./log.txt");
+            List<String> lines = Files.readAllLines(filePath);
+            System.out.println(makeIpList(lines));
+
         } catch (IOException e) {
             System.out.println("unidentified file path");
         }
+    }
+    public static String[] makeIpList(List list) {
+        String[] ipList = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+          String temp = (String)list.get(i);
+            ipList = temp.split("   ");
+
+        } return ipList;
+
     }
 }
