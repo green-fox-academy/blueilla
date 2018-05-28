aeadb434278564753a2529205ca8ce451fd93003
blueilla <jasenszky.ilona@gmail.com>
2018-05-28 22:32:46 +0200

    feature(Homework)person and student classes done

---
+++ week-04/day-02/GreenFoxHomework/src/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+
+    String name;
+    int age;
+    String gender;
+
+
+    public String introduce() {
+        return "Hi, I'm " + name + ", a" + age + " year old" + gender + ".";
+    }
+    public String getGoal() {
+        return "My goal is, live for the moment!";
+    }
+    public Person(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    public Person() {
+        name = "Jane Doe";
+        age = 30;
+        gender = "female";
+
+    }
+}
---
+++ week-04/day-02/GreenFoxHomework/src/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+
+    String name;
+    int age;
+    String gender;
+    String previousOrganization;
+    int skippedDays;
+    //int numberOfDays;
+
+
+    public String introduce() {
+        return "Hi, I'm " + name + ", a" + age + " year old" + gender + " from" + previousOrganization + " who skipped" + skippedDays + " days from the course already.";
+    }
+    public String getGoal() {
+        return "Be a junior software developer!";
+    }
+    public int skipDays(int numberOfDays) {
+        skippedDays += numberOfDays;
+        return numberOfDays;
+    }
+    public Student(String name, int age, String gender, String previousOrganization) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.previousOrganization = previousOrganization;
+        skippedDays = 0;
+    }
+    public Student() {
+        name = "Jane Doe";
+        age = 30;
+        gender = "female";
+        previousOrganization = "The School of life";
+        skippedDays = 0;
+
+    }
+
+}
