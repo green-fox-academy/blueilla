223f6cafb09a48569a2247f356097d2af0d40aca
blueilla <jasenszky.ilona@gmail.com>
2018-05-29 16:21:31 +0200

    feat(GardeningApp):set flower and tree classes, and trying some watering function

--- week-04/day-02/GardenApplication/src/Flower.java
+++ week-04/day-02/GardenApplication/src/Flower.java
@@ -1,2 +1,40 @@
 public class Flower {
+
+ /* needs water if its current water amount is less then 5
+  when watering it the flower can only absorb the 75% of the water
+  eg. watering with 10 the flower's amount of water should only increase with 7.5 */
+
+ private int waterAmount;
+ private String color;
+
+ public Flower(String color, int waterAmount) {
+   this.color = color;
+   this.waterAmount = waterAmount;
+ }
+
+  public int getWaterAmount() {
+    return waterAmount;
+  }
+
+  public void setWaterAmount(int waterAmount) {
+    this.waterAmount = waterAmount;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public boolean flowerNeedsWater(int waterAmount) {
+   if (waterAmount < 5) {
+     System.out.println("The Flower needs water!");
+     return true;
+   } else {
+     System.out.println("The Flower does not need water.");
+     return false;
+   }
+  }
 }
--- week-04/day-02/GardenApplication/src/Tree.java
+++ week-04/day-02/GardenApplication/src/Tree.java
@@ -1,2 +1,39 @@
 public class Tree {
+  /* needs water if its current water amount is less then 10
+  when watering it the tree can only absorb the 40% of the water
+  eg. watering with 10 the tree's amount of water should only increase with 4 */
+
+  private int waterAmount;
+  private String color;
+
+  public Tree(String color, int waterAmount) {
+    this.color = color;
+    this.waterAmount = waterAmount;
+  }
+
+  public int getWaterAmount() {
+    return waterAmount;
+  }
+
+  public void setWaterAmount(int waterAmount) {
+    this.waterAmount = waterAmount;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public boolean treeNeedsWater(int waterAmount) {
+    if (waterAmount < 10) {
+      System.out.println("The Flower needs water!");
+      return true;
+    } else {
+      System.out.println("The Flower does not need water.");
+      return false;
+    }
+  }
 }
