53fc44af9b5d1359b641b3dec00c5cb32fb2b821
blueilla <jasenszky.ilona@gmail.com>
2018-05-29 16:32:09 +0200

    feat(GardeningApp): create some Flowers and Trees in List

--- week-04/day-02/GardenApplication/src/GardenApp.java
+++ week-04/day-02/GardenApplication/src/GardenApp.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class GardenApp {
+
+  public static void main(String[] args) {
+
+    ArrayList<Flower> flowers = new ArrayList<>();
+    ArrayList<Tree> trees = new ArrayList<>();
+
+    Flower yellowFlower = new Flower("yellow", 4);
+    flowers.add(yellowFlower);
+    Flower blueFlower = new Flower("blue", 5);
+    flowers.add(blueFlower);
+
+    Tree purpleTree = new Tree("purple", 15);
+    trees.add(purpleTree);
+    Tree orangeTree = new Tree("orange", 8);
+    trees.add(orangeTree);
+  }
 }
