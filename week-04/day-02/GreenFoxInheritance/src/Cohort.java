c7d72952f387f3151ba3c5aca97139718d06782f
blueilla <jasenszky.ilona@gmail.com>
2018-05-29 13:54:18 +0200

    feat(GF Inheritance): Cohort class added

--- week-04/day-02/GreenFoxInheritance/src/Cohort.java
+++ week-04/day-02/GreenFoxInheritance/src/Cohort.java
@@ -1,2 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cohort {
+
+  private String name;
+  private List students;
+  private List mentors;
+
+  public Cohort(String name) {
+    this.name = name;
+    students = new ArrayList();
+    mentors = new ArrayList();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List getStudents() {
+    return students;
+  }
+
+  public void setStudents(List students) {
+    this.students = students;
+  }
+
+  public List getMentors() {
+    return mentors;
+  }
+
+  public void setMentors(List mentors) {
+    this.mentors = mentors;
+  }
+
+  public void addStudent(Student student) {
+    students.add(student);
+  }
+
+  public void addMentor(Mentor mentor) {
+    mentors.add(mentor);
+  }
+
+  public void info() {
+    System.out.println("The " + name + " cohort has " + students.size() +
+            " students and " + mentors.size() + " mentors.");
+  }
 }
