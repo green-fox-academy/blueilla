8f1ca4162de4b01fc60521d698c6ba041ad7fa30
blueilla <jasenszky.ilona@gmail.com>
2018-06-18 14:35:11 +0200

    Ex02 ex done

---
+++ week-06/day-01/src/Ex02.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ex02 {
+
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+    System.out.println(getOddAverage(numbers));
+  }
+
+  private static double getOddAverage(ArrayList<Integer> numbers) {
+    return numbers.stream()
+            .filter(n -> n % 2 == 1)
+            .mapToInt(n -> n)
+            .average()
+            .getAsDouble();
+  }
+}
