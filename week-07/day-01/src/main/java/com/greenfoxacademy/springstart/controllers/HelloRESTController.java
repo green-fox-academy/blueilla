2502aa656c9a5c989c65ab22a6919de0a4d573dd
blueilla <jasenszky.ilona@gmail.com>
2018-06-25 14:02:52 +0200

    add incrementing counter to the grreting function

--- week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
 
+  AtomicLong idCounter = new AtomicLong();
+
   @RequestMapping(value="/greeting")
   public Greeting greetingFunction(@RequestParam(value="name") String name) {
-    Greeting greeting = new Greeting(1, "Hello " + name + "!");
+    Greeting greeting = new Greeting(idCounter.incrementAndGet(), "Hello " + name + "!");
     return greeting;
   }
 }
