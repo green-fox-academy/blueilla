8f30449e700d2625c656377e4e6e048ce15abc8c
blueilla <jasenszky.ilona@gmail.com>
2018-06-25 13:59:13 +0200

    greeting class refactored

--- week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,15 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @RequestMapping(value="/greeting")
-  public Greeting greetingFunction() {
-    Greeting greeting = new Greeting(1, "Hello World!");
+  public Greeting greetingFunction(@RequestParam(value="name") String name) {
+    Greeting greeting = new Greeting(1, "Hello " + name + "!");
     return greeting;
   }
 }
