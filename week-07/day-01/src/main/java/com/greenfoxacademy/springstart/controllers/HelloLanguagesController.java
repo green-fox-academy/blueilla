1d4ae7a1fd2bdca43ff95a29b1a19a0c8182aa59
blueilla <jasenszky.ilona@gmail.com>
2018-06-26 11:13:25 +0200

    small modification

--- week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloLanguagesController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloLanguagesController.java
@@ -4,15 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloLanguagesController {
 
-  AtomicLong loadCounter = new AtomicLong();
 
   private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
           "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
