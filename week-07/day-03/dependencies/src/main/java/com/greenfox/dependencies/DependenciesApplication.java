ff5c22537297fbcac747f7b80a699ce0bf707d21
blueilla <jasenszky.ilona@gmail.com>
2018-06-27 11:49:59 +0200

    (dependencies) first exercise

--- week-07/day-03/dependencies/src/main/java/com/greenfox/dependencies/DependenciesApplication.java
+++ week-07/day-03/dependencies/src/main/java/com/greenfox/dependencies/DependenciesApplication.java
@@ -1,12 +1,24 @@
 package com.greenfox.dependencies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DependenciesApplication {
+public class DependenciesApplication implements CommandLineRunner {
+
+  @Autowired
+  Printer printer;
 
   public static void main(String[] args) {
     SpringApplication.run(DependenciesApplication.class, args);
+
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("HELLO");
+
   }
 }
