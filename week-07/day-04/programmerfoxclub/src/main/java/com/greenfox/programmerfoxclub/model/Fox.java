cac50fec99f30be05199f5fdc311e385d31ebbaa
blueilla <jasenszky.ilona@gmail.com>
2018-06-28 14:14:29 +0200

    PF Club: create fox class

--- week-07/day-04/programmerfoxclub/src/main/java/com/greenfox/programmerfoxclub/model/Fox.java
+++ week-07/day-04/programmerfoxclub/src/main/java/com/greenfox/programmerfoxclub/model/Fox.java
@@ -1,5 +1,21 @@
 package com.greenfox.programmerfoxclub.model;
 
+import java.util.ArrayList;
+
 public class Fox {
 
+  private String name;
+  private ArrayList<String> foods;
+  private ArrayList<String> drinks;
+  private ArrayList<String> tricks;
+
+  public Fox() {
+  }
+
+  public Fox(String name) {
+    this.name = name;
+    this.foods = new ArrayList<>();
+    this.drinks = new ArrayList<>();
+    this.tricks = new ArrayList<>();
+  }
 }
