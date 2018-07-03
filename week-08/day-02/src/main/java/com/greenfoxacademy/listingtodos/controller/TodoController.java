b137558af714c7698e064629632d675ce854113a
blueilla <jasenszky.ilona@gmail.com>
2018-07-03 13:53:30 +0200

    feat(listing todos)controllers add to / and /list

---
+++ week-08/day-02/src/main/java/com/greenfoxacademy/listingtodos/controller/TodoController.java
@@ -0,0 +1,23 @@
+package com.greenfoxacademy.listingtodos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TodoController {
+
+  @GetMapping(value={"/", "/list"})
+  @ResponseBody
+  public String list(Model model) {
+    model.addAttribute("todo");
+    return ("This is my first todo!");
+  }
+
+
+
+}
+
+
+
