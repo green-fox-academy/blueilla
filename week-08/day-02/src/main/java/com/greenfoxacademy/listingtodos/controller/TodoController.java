382a567c67ff860c1a236df22fb586a895249f8b
blueilla <jasenszky.ilona@gmail.com>
2018-07-03 16:10:08 +0200

    feat(listing todos ex)create todoRepository class and autowired it

--- week-08/day-02/src/main/java/com/greenfoxacademy/listingtodos/controller/TodoController.java
+++ week-08/day-02/src/main/java/com/greenfoxacademy/listingtodos/controller/TodoController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.listingtodos.controller;
 
+import com.greenfoxacademy.listingtodos.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
+
+  @Autowired
+  TodoRepository todoRepository;
+
+  public TodoController(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
 
   @GetMapping(value={"/", "/list"})
   @ResponseBody
--- week-08/day-02/src/main/java/com/greenfoxacademy/listingtodos/repository/TodoRepository.java
+++ week-08/day-02/src/main/java/com/greenfoxacademy/listingtodos/repository/TodoRepository.java
@@ -2,6 +2,8 @@ package com.greenfoxacademy.listingtodos.repository;
 
 import com.greenfoxacademy.listingtodos.model.Todo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
