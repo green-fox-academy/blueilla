26b63a22bd12d4a8efd716677a1a374231a967a2
blueilla <jasenszky.ilona@gmail.com>
2018-07-03 20:14:30 +0200

    redone and rebuild the whole exercise after ruin the filestructure

--- week-08/day-02/connect-with-mysql/src/main/java/com/greenfoxacademy/connectionwithmysql/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenfoxacademy.connectionwithmysql.repository;
-
-import com.greenfoxacademy.connectionwithmysql.model.Todo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends CrudRepository<Todo, Long> {
-}
