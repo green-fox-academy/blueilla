846f8c55e9853c484aaa98cedcbf795a42b21d22
blueilla <jasenszky.ilona@gmail.com>
2018-07-03 13:19:08 +0200

    feat(listing todos) set up Todo class

---
+++ week-08/day-02/src/main/java/com/greenfoxacademy/listingtodos/model/Todo.java
@@ -0,0 +1,50 @@
+package com.greenfoxacademy.listingtodos.model;
+
+public class Todo {
+
+  private long id;
+  private String title;
+  private boolean urgent;
+  private boolean done;
+
+  public Todo() {}
+
+  public Todo(long id, String title) {
+    this.id = id;
+    this.title = title;
+    urgent = false;
+    done = false;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isUrgent() {
+    return urgent;
+  }
+
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+}
