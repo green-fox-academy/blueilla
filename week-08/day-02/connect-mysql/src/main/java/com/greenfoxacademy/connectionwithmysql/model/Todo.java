cc886c606492d1c2a572cf090c2456e8a42b8be7
blueilla <jasenszky.ilona@gmail.com>
2018-07-04 15:16:43 +0200

    small modifications in exercie

--- week-08/day-02/connect-mysql/src/main/java/com/greenfoxacademy/connectionwithmysql/model/Todo.java
+++ week-08/day-02/connect-mysql/src/main/java/com/greenfoxacademy/connectionwithmysql/model/Todo.java
@@ -11,7 +11,7 @@ public class Todo {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private long id;
+  private Long id;
   private String title;
   private boolean urgent;
   private boolean done;
@@ -24,8 +24,12 @@ public class Todo {
     done = false;
   }
 
-  public long getId() {
+  public Long getId() {
     return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 
   public String getTitle() {
