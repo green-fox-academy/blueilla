c58f01526f85d3338b876f253e49968c6923a8f6
blueilla <jasenszky.ilona@gmail.com>
2018-07-10 17:15:05 +0200

    init(grootproject)set up the environment

---
+++ week-09/day-02/GrootProject/src/main/java/com/greenfoxacademy/grootproject/GrootprojectApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.grootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrootprojectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GrootprojectApplication.class, args);
+  }
+}
